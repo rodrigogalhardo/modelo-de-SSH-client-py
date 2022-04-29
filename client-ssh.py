3fcea62b04af38fb2962497e4b5e26f900c6da49
Rodrigo Galhardo <6187392+rodrigogalhardo@users.noreply.github.com>
2022-04-29 20:21:32 -0300

    Create client-ssh.py

---
+++ client-ssh.py
@@ -0,0 +1,26 @@
+"""
+Cliente SSH Py - version 1.0
+By R Galhardo | CISO, Ethical Hacking
+"""
+
+import paramiko
+print('=====================================')
+print('#             SSH Client            #')
+print('=====================================')
+HOST = input('HOST:')
+USER = input('USER:')
+PASSWD = input('PASSWORD:')
+try:
+    client_ssh = paramiko.SSHClient()
+    client_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client_ssh.connect(hostname=HOST, username=USER, password=PASSWD)
+except Exception as err:
+    print(err)
+
+while True:
+    stdin, stdout, stderr = client_ssh.exec_command(input('(bash):'))
+    for linha in stdout.readlines():
+        if stderr:
+            print(stderr)
+        else:
+            print(linha)
